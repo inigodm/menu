ac09086c0264ca492e45bf19adc6a40d3ac81ede
inigo <inigo.delgado@gmail.com>
2019-06-24 21:15:45 +0200

    Lists

--- src/main/kotlin/Writer.kt
+++ src/main/kotlin/Writer.kt
@@ -6,4 +6,8 @@ class Writer (var out: PrintStream = System.out){
     fun write(s: String){
         out.println(s)
     }
+
+    fun write(s: Array<String>) {
+        s.forEach { out.println(it) }
+    }
 }
--- src/test/kotlin/MenuTest.kt
+++ src/test/kotlin/MenuTest.kt
@@ -1,15 +1,30 @@
 import org.junit.Assert.*
+import org.junit.Before
 import org.junit.Test
+import org.mockito.ArgumentMatchers
 import java.io.PrintStream
 import org.mockito.Mockito.*
 
 class steps{
-    var out: PrintStream = spy(System.out)
+    lateinit var out: PrintStream
+    @Before
+    fun setup(){
+        out = spy(System.out)
+    }
 
     @Test
     fun write_something_in_io(){
         var w = Writer(out)
+        println(out)
         w.write("text")
         verify(out, only()).println("text")
+    }
+
+    @Test
+    fun write_a_list_of_things(){
+        var w = Writer(out)
+        println(out)
+        w.write(arrayOf("text1", "text2"))
+        verify(out, times(2)).println(ArgumentMatchers.anyString())
     }
 }
